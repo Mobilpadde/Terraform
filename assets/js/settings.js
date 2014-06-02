acc7e73d452211de1590d6aef02cd335367bbab8
Mads Cordes <Mobilpadde@gmail.com>
2014-06-02 22:42:56 +0200

    Capital letter

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,13 +1,13 @@
 settings = {
 	colours: {
-		player: ["#ac0ff0", "#ca1c79", "#f00f53", "#e006a6", "#d5c72a", "#d42b55", "#ff008c"],
+		player: ["#ac0ff0", "#ca1c79", "#f00f53", "#e006a6", "#d5c72a", "#d42b55", "#ff008c", "#11eec2"],
 		scheme: {
 			0: ["#3c98c3", "sky"], 		// Untouchable
 			1: ["#0ef192", "Grass"], 	// Grows on the gorund
 			2: ["#b3664d", "Ground"], 	// Does not fall down
 			3: ["#3c45c3", "Water"],	// Does whatever water does
 			4: ["#ffcc00", "Sand"],		// Falls down
-			5: ["#333333", "avlsidian"] // Undestructable
+			5: ["#333333", "Avlsidian"] // Undestructable
 		}
 	},
 	sizes: {
